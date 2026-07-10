4e62c07e4693f5ec619622190fb6bbfa4c9070f5
mcdcndmadrid-oss <mcdc.nd.madrid@gmail.com>
2026-07-10 12:40:04 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* UrbanTrace — service worker: cache-first para funcionar sin conexión */
-var CACHE = 'urbantrace-v14';
+var CACHE = 'urbantrace-v15';
 var ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function (e) {
